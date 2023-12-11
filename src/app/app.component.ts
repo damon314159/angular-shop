10d65cfab8e4c0c27d5973b2c27e8841919e17d5
Damon <126731021+damon314159@users.noreply.github.com>
2023-12-11 13:52:21 +0000

    Create simple welcome page component
    Temporarily hardcode the selector under router-outlet in place of future
    routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,20 @@ import { MatIconModule } from '@angular/material/icon'
 import { MatButtonModule } from '@angular/material/button'
 import { MatToolbarModule } from '@angular/material/toolbar'
 import { MatMenuModule } from '@angular/material/menu'
+import { WelcomeComponent } from './home/welcome.component'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MatToolbarModule, MatButtonModule, MatIconModule, MatMenuModule],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule,
+    WelcomeComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
