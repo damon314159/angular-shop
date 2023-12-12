34a40559af20ac3f1cbc0da7fa0ee55be51bac19
Damon <126731021+damon314159@users.noreply.github.com>
2023-12-12 13:51:41 +0000

    Template change detection bug resolved

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { type Routes } from '@angular/router'
+import { WelcomeComponent } from './home/welcome.component'
 
-export const routes: Routes = []
+export const routes: Routes = [
+  { path: 'welcome', component: WelcomeComponent },
+  { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+  { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
+]
--- src/app/home/welcome.component.ts
+++ src/app/home/welcome.component.ts
@@ -1,56 +1,15 @@
 import { CommonModule } from '@angular/common'
-import { Component, HostListener, type TemplateRef, ViewChild } from '@angular/core'
+import { Component, HostListener } from '@angular/core'
 import { MatGridListModule } from '@angular/material/grid-list'
 
-export interface Tile {
-  name: string
-  template: TemplateRef<any>
-  cols: number
-  rows: number
-}
-
 @Component({
-  selector: 'app-welcome',
   standalone: true,
   imports: [MatGridListModule, CommonModule],
   templateUrl: './welcome.component.html',
   styleUrl: './welcome.component.scss'
 })
 export class WelcomeComponent {
-  @ViewChild('tileOne') tileOneTemplate!: TemplateRef<any>
-  @ViewChild('tileTwo') tileTwoTemplate!: TemplateRef<any>
-  @ViewChild('tileThree') tileThreeTemplate!: TemplateRef<any>
-
-  tiles: Tile[] = [
-    { name: 'tileOne', template: this.tileOneTemplate, cols: 1, rows: 1 },
-    { name: 'tileTwo', template: this.tileTwoTemplate, cols: 1, rows: 2 },
-    { name: 'tileThree', template: this.tileThreeTemplate, cols: 1, rows: 1 }
-  ]
-
   cols!: number
-
-  getTileTemplate(tile: Tile): TemplateRef<any> {
-    return tile.template
-  }
-
-  ngAfterViewInit(): void {
-    this.tiles.forEach((tile) => {
-      tile.template = this.getTemplateByName(tile.name)
-    })
-  }
-
-  private getTemplateByName(name: string): TemplateRef<any> {
-    switch (name) {
-      case 'tileOne':
-        return this.tileOneTemplate
-      case 'tileTwo':
-        return this.tileTwoTemplate
-      case 'tileThree':
-        return this.tileThreeTemplate
-      default:
-        return this.tileOneTemplate
-    }
-  }
 
   calculateCols(): number {
     const windowWidth = window.innerWidth
