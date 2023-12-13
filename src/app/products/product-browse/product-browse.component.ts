84842d8a2a2d3c52414dea7db0630b79b50c97fa
Damon <126731021+damon314159@users.noreply.github.com>
2023-12-13 15:06:26 +0000

    Simple product display with searchbar filter
    Temporarily hardcoded product info in place of http request

--- src/app/products/product-browse/product-browse.component.ts
+++ src/app/products/product-browse/product-browse.component.ts
@@ -1,10 +1,73 @@
 import { Component } from '@angular/core'
+import { CommonModule, CurrencyPipe } from '@angular/common'
+import { MatButtonModule } from '@angular/material/button'
+import { MatCardModule } from '@angular/material/card'
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatInputModule } from '@angular/material/input'
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatIconModule } from '@angular/material/icon'
+import { FormsModule } from '@angular/forms'
 
 @Component({
-  selector: 'app-product-browse',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatInputModule,
+    MatFormFieldModule,
+    CurrencyPipe,
+    MatIconModule,
+    FormsModule
+  ],
   templateUrl: './product-browse.component.html',
   styleUrl: './product-browse.component.scss'
 })
-export class ProductBrowseComponent {}
+export class ProductBrowseComponent {
+  #filter = ''
+  get filter(): string {
+    return this.#filter
+  }
+
+  set filter(arg: string) {
+    this.#filter = arg
+    this.relevantProducts = this.products.filter((product) => product.name.toLowerCase().includes(arg.toLowerCase()))
+  }
+
+  category = 'shirts'
+  products = [
+    {
+      id: 1,
+      name: 'Be Curious Shirt',
+      image: 'images/beCuriousShirt.png',
+      price: 50
+    },
+    {
+      id: 2,
+      name: 'Creator Shirt',
+      image: 'images/creatorShirt.png',
+      price: 35
+    },
+    {
+      id: 3,
+      name: 'Doer Shirt',
+      image: 'images/doerShirt.png',
+      price: 35
+    },
+    {
+      id: 4,
+      name: 'Translator Shirt',
+      image: 'images/translatorShirt.png',
+      price: 35
+    },
+    {
+      id: 5,
+      name: 'Pro Shirt',
+      image: 'images/proShirt.png',
+      price: 60
+    }
+  ]
+
+  relevantProducts = this.products
+}
