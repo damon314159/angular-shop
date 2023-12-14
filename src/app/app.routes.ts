ff39d4a994b39afb7f5a007fb1b458a9f67d01cd
Damon <126731021+damon314159@users.noreply.github.com>
2023-12-14 18:49:52 +0000

    Add checkout routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { type Routes } from '@angular/router'
 import { WelcomeComponent } from './home/welcome.component'
 import { ProductBrowseComponent } from './products/product-browse/product-browse.component'
+import { CheckoutComponent } from './checkout/checkout.component'
 
 export const routes: Routes = [
   { path: 'welcome', component: WelcomeComponent },
   { path: 'catalogue', component: ProductBrowseComponent },
+  { path: 'shopping-cart', component: CheckoutComponent },
   { path: '', redirectTo: 'welcome', pathMatch: 'full' },
   { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
 ]
