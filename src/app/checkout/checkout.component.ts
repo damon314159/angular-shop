806b01a9ea0f7dbd86d5a65e9aa8d917efb3de46
Damon <126731021+damon314159@users.noreply.github.com>
2023-12-14 18:50:51 +0000

    Create cart view table with calculator

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -1,10 +1,62 @@
-import { Component } from '@angular/core'
+import { Component, ViewChild } from '@angular/core'
+import { CurrencyPipe } from '@angular/common'
+import { MatTableModule, MatTable } from '@angular/material/table'
+import { MatCardModule } from '@angular/material/card'
+import { MatButtonModule } from '@angular/material/button'
+import { MatExpansionModule } from '@angular/material/expansion'
+import { MatIconModule } from '@angular/material/icon'
+import { CartService, type Transaction } from '../services/cart.service'
 
 @Component({
   selector: 'app-checkout',
   standalone: true,
-  imports: [],
+  imports: [MatTableModule, CurrencyPipe, MatCardModule, MatButtonModule, MatExpansionModule, MatIconModule],
   templateUrl: './checkout.component.html',
   styleUrl: './checkout.component.scss'
 })
-export class CheckoutComponent {}
+export class CheckoutComponent {
+  constructor(private readonly cartService: CartService) {}
+
+  displayedColumns: string[] = ['item', 'quantity', 'price']
+  @ViewChild(MatTable) table!: MatTable<any>
+
+  transactions: Transaction[] = []
+
+  ngOnInit(): void {
+    this.transactions = this.cartService.getCartItems()
+  }
+
+  getTotalPrice(): number {
+    return this.transactions.reduce((acc, t) => acc + t.quantity * t.price, 0)
+  }
+
+  getTotalQuantity(): number {
+    return this.transactions.map((t) => t.quantity).reduce((acc, value) => acc + value, 0)
+  }
+
+  getTax(): number {
+    return this.getTotalPrice() * 0.1
+  }
+
+  getShipping(): number {
+    return 20
+  }
+
+  getTotal(): number {
+    return this.getTotalPrice() + this.getTax() + this.getShipping()
+  }
+
+  plusQuantity(id: number): void {
+    const item = this.transactions.find((t) => t.id === id)!
+    this.cartService.addToCart(item)
+    this.transactions = this.cartService.getCartItems()
+    this.table.renderRows()
+  }
+
+  minusQuantity(id: number): void {
+    const item = this.transactions.find((t) => t.id === id)!
+    this.cartService.removeFromCart(item)
+    this.transactions = this.cartService.getCartItems()
+    this.table.renderRows()
+  }
+}
