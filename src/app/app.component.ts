6f0e12eed3825dce921efb5eed77239711eae562
Damon <126731021+damon314159@users.noreply.github.com>
2023-12-19 13:22:27 +0000

    Badge on cart nav button showing current quantity

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,16 +5,30 @@ import { MatIconModule } from '@angular/material/icon'
 import { MatButtonModule } from '@angular/material/button'
 import { MatToolbarModule } from '@angular/material/toolbar'
 import { MatMenuModule } from '@angular/material/menu'
+import { MatBadgeModule } from '@angular/material/badge'
+import { CartService } from './services/cart.service'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MatToolbarModule, MatButtonModule, MatIconModule, MatMenuModule],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule,
+    MatBadgeModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  constructor(private readonly router: Router) {}
+  constructor(private readonly router: Router, private readonly cartService: CartService) {}
+
+  getTotalQuantity(): number {
+    return this.cartService.getTotalQuantity()
+  }
 
   async onHomeNav(): Promise<void> {
     await this.router.navigate(['/welcome'])
